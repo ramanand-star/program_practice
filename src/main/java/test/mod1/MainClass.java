03650a40526a5dfdd20eddf3db243b48efb29434
Ramanand Kumar <ramanand_kumar@trimble.com>
2021-04-25 05:33:33 +0530

    first commit ..

--- src/main/java/test/mod1/MainClass.java
+++ src/main/java/test/mod1/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
         printName("Ramanand");
     }
     public static void printName(String name){
-        System.out.println("Hello Mr "+name);
+        System.out.println("Hello  Mr "+name);
     }
 
 }
